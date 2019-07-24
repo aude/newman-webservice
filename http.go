d952fb911d2cbc6f0f6eccc62f228234afacb8e2
Asbjørn Apeland <aude@users.noreply.github.com>
2019-07-24 16:43:26 +0200

    split into several files

---
+++ http.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"regexp"
+)
+
+func helpHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: add help message
+	http.Error(w, "not implemented: help", http.StatusNotImplemented)
+}
+
+func collectionsHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+var validId     = regexp.MustCompile("^[0-9A-Za-z-]+$")
+var validAPIKey = regexp.MustCompile("^[0-9a-z]+$")
+
+func getQueryParameter(_url *url.URL, name string) (value string, err error) {
+	query, err := url.ParseQuery(_url.RawQuery)
+	if err != nil {
+		return "", err
+	}
+	if _, ok := query[name]; !ok {
+		return "", fmt.Errorf("query parameter missing: %s", name)
+	}
+	if len(query[name]) > 1 {
+		return "", fmt.Errorf("more than one query parameter with the same name: %s", name)
+	}
+
+	value = query[name][0]
+
+	return value, nil
+}
+
+func collectionHandler(w http.ResponseWriter, r *http.Request) {
+	// get collection ID
+	collectionID := r.URL.Path[len("/collections/"):]
+	if ! validId.MatchString(collectionID) {
+		http.Error(w, "invalid collection ID", http.StatusBadRequest)
+		return
+	}
+
+	// get environment ID
+	environmentID, err := getQueryParameter(r.URL, "environment")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if ! validId.MatchString(environmentID) {
+		http.Error(w, "invalid environment ID", http.StatusBadRequest)
+		return
+	}
+
+	// get API key
+	apiKey, err := getQueryParameter(r.URL, "apikey")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if ! validAPIKey.MatchString(apiKey) {
+		http.Error(w, "invalid API key", http.StatusBadRequest)
+		return
+	}
+
+	report := "mock\n"
+
+	fmt.Fprint(w, report)
+}
--- main.go
+++ main.go
@@ -1,74 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"regexp"
 )
-
-func helpHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: add help message
-	http.Error(w, "not implemented: help", http.StatusNotImplemented)
-}
-
-func collectionsHandler(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, "not implemented", http.StatusNotImplemented)
-}
-
-var validId     = regexp.MustCompile("^[0-9A-Za-z-]+$")
-var validAPIKey = regexp.MustCompile("^[0-9a-z]+$")
-
-func getQueryParameter(_url *url.URL, name string) (value string, err error) {
-	query, err := url.ParseQuery(_url.RawQuery)
-	if err != nil {
-		return "", err
-	}
-	if _, ok := query[name]; !ok {
-		return "", fmt.Errorf("query parameter missing: %s", name)
-	}
-	if len(query[name]) > 1 {
-		return "", fmt.Errorf("more than one query parameter with the same name: %s", name)
-	}
-
-	value = query[name][0]
-
-	return value, nil
-}
-
-func collectionHandler(w http.ResponseWriter, r *http.Request) {
-	// get collection ID
-	collectionID := r.URL.Path[len("/collections/"):]
-	if ! validId.MatchString(collectionID) {
-		http.Error(w, "invalid collection ID", http.StatusBadRequest)
-		return
-	}
-
-	// get environment ID
-	environmentID, err := getQueryParameter(r.URL, "environment")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if ! validId.MatchString(environmentID) {
-		http.Error(w, "invalid environment ID", http.StatusBadRequest)
-		return
-	}
-
-	// get API key
-	apiKey, err := getQueryParameter(r.URL, "apikey")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if ! validAPIKey.MatchString(apiKey) {
-		http.Error(w, "invalid API key", http.StatusBadRequest)
-		return
-	}
-
-	fmt.Fprintf(w, "so, you wanna access %v, %v, %v?\n", collectionID, environmentID, apiKey)
-}
 
 func main() {
 	http.HandleFunc("/", helpHandler)
