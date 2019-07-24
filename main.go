8cab36f1522b478ce01572fb022eaf120a41cc9d
Asbjørn Apeland <aude@users.noreply.github.com>
2019-07-24 15:13:23 +0200

    add apikey consumption

--- main.go
+++ main.go
@@ -17,7 +17,8 @@ func collectionsHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "not implemented", http.StatusNotImplemented)
 }
 
-var validId = regexp.MustCompile("^[0-9A-Za-z-]+$")
+var validId     = regexp.MustCompile("^[0-9A-Za-z-]+$")
+var validAPIKey = regexp.MustCompile("^[0-9a-z]+$")
 
 func getQueryParameter(_url *url.URL, name string) (value string, err error) {
 	query, err := url.ParseQuery(_url.RawQuery)
@@ -25,10 +26,10 @@ func getQueryParameter(_url *url.URL, name string) (value string, err error) {
 		return "", err
 	}
 	if _, ok := query[name]; !ok {
-		return "", fmt.Errorf("query parameter missing: environment")
+		return "", fmt.Errorf("query parameter missing: %s", name)
 	}
 	if len(query[name]) > 1 {
-		return "", fmt.Errorf("more than one query parameter with the same name: environment")
+		return "", fmt.Errorf("more than one query parameter with the same name: %s", name)
 	}
 
 	value = query[name][0]
@@ -55,7 +56,18 @@ func collectionHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "so, you wanna access %v, %v?\n", collectionID, environmentID)
+	// get API key
+	apiKey, err := getQueryParameter(r.URL, "apikey")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if ! validAPIKey.MatchString(apiKey) {
+		http.Error(w, "invalid API key", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "so, you wanna access %v, %v, %v?\n", collectionID, environmentID, apiKey)
 }
 
 func main() {
