15c314f600abf8cc140c5da5f81fc47bce097f45
Asbjørn Apeland <aude@users.noreply.github.com>
2019-07-24 15:09:05 +0200

    add main routes

--- main.go
+++ main.go
@@ -4,13 +4,63 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"regexp"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "So, you wanna access %s?\n", r.URL.Path[0:])
+func helpHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: add help message
+	http.Error(w, "not implemented: help", http.StatusNotImplemented)
+}
+
+func collectionsHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "not implemented", http.StatusNotImplemented)
+}
+
+var validId = regexp.MustCompile("^[0-9A-Za-z-]+$")
+
+func getQueryParameter(_url *url.URL, name string) (value string, err error) {
+	query, err := url.ParseQuery(_url.RawQuery)
+	if err != nil {
+		return "", err
+	}
+	if _, ok := query[name]; !ok {
+		return "", fmt.Errorf("query parameter missing: environment")
+	}
+	if len(query[name]) > 1 {
+		return "", fmt.Errorf("more than one query parameter with the same name: environment")
+	}
+
+	value = query[name][0]
+
+	return value, nil
+}
+
+func collectionHandler(w http.ResponseWriter, r *http.Request) {
+	// get collection ID
+	collectionID := r.URL.Path[len("/collections/"):]
+	if ! validId.MatchString(collectionID) {
+		http.Error(w, "invalid collection ID", http.StatusBadRequest)
+		return
+	}
+
+	// get environment ID
+	environmentID, err := getQueryParameter(r.URL, "environment")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if ! validId.MatchString(environmentID) {
+		http.Error(w, "invalid environment ID", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "so, you wanna access %v, %v?\n", collectionID, environmentID)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", helpHandler)
+	http.HandleFunc("/collections", collectionsHandler)
+	http.HandleFunc("/collections/", collectionHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
