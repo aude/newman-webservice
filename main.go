159c2f3bbdf4f705c2e5814222bb036c706c8de6
Asbjørn Apeland <aude@users.noreply.github.com>
2019-07-24 10:29:09 +0200

    add HTTP hello world

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "So, you wanna access %s?\n", r.URL.Path[0:])
+}
+
 func main() {
-	println("Hello, world!")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
