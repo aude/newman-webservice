d20a9215d90a6bc2cb589bbbbd0ae3e51dd0655d
Asbjørn Apeland <aude@users.noreply.github.com>
2019-07-25 15:43:21 +0200

    organize

--- http.go
+++ http.go
@@ -20,23 +20,6 @@ func collectionsHandler(w http.ResponseWriter, r *http.Request) {
 var validId     = regexp.MustCompile("^[0-9A-Za-z-]+$")
 var validAPIKey = regexp.MustCompile("^[0-9a-z]+$")
 
-func getQueryParameter(_url *url.URL, name string) (value string, err error) {
-	query, err := url.ParseQuery(_url.RawQuery)
-	if err != nil {
-		return "", err
-	}
-	if _, ok := query[name]; !ok {
-		return "", fmt.Errorf("query parameter missing: %s", name)
-	}
-	if len(query[name]) > 1 {
-		return "", fmt.Errorf("more than one query parameter with the same name: %s", name)
-	}
-
-	value = query[name][0]
-
-	return value, nil
-}
-
 func collectionHandler(w http.ResponseWriter, r *http.Request) {
 	// get collection ID
 	collectionID := r.URL.Path[len("/collections/"):]
@@ -75,4 +58,21 @@ func collectionHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, report)
+}
+
+func getQueryParameter(_url *url.URL, name string) (value string, err error) {
+	query, err := url.ParseQuery(_url.RawQuery)
+	if err != nil {
+		return "", err
+	}
+	if _, ok := query[name]; !ok {
+		return "", fmt.Errorf("query parameter missing: %s", name)
+	}
+	if len(query[name]) > 1 {
+		return "", fmt.Errorf("more than one query parameter with the same name: %s", name)
+	}
+
+	value = query[name][0]
+
+	return value, nil
 }
